053227dce8f33d47fe5a512dac053b2b9d371594
Jett <jettc505@gmail.com>
2023-05-31 15:01:31 -0600

    Finished headder and navagation

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,38 @@
 <script lang="ts">
-    import Header from "./components/Header.svelte";    
+    import Header from "./components/Header.svelte";
+    import Footer from "./components/Footer.svelte";
+    import NavePopout from "./components/navePopout/NavePopout.svelte";
+
+    let tabs = ["Home", "Order", "Price List"];
+    let activeItem = tabs[0];
+    let showNave = false;
+
+    function tabChange(e) {
+        activeItem = e.detail;
+        toggleNave();
+    }
+    function toggleNave() {
+        showNave = !showNave;
+    }
 </script>
 
 <main>
-    <Header />	
+    <NavePopout
+        {showNave}
+        items={tabs}
+        {activeItem}
+        on:tabChange={tabChange}
+        on:click={toggleNave}
+    />
+    <Header on:click={toggleNave} />
+    {#if activeItem === "Home"}
+        <div>Home</div>
+    {:else if activeItem === "Order"}
+        <div>Order</div>
+    {:else if activeItem === "Price List"}
+        <div>Price List</div>
+    {/if}
+    <Footer />
 </main>
 
 <style>
