97267e6859bf8af73989fc32f08fde17ef4ab0c9
Jett <jettc505@gmail.com>
2023-06-01 18:26:18 -0600

    fineshed home page

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,9 @@
     import Footer from "./components/Footer.svelte";
     import NavePopout from "./components/navePopout/NavePopout.svelte";
 
+    // pages
+    import Home from "./pages/Home.svelte";
+
     let tabs = ["Home", "Order", "Price List"];
     let activeItem = tabs[0];
     let showNave = false;
@@ -10,6 +13,7 @@
     function tabChange(e) {
         activeItem = e.detail;
         toggleNave();
+        console.log(typeof e.detail);
     }
     function toggleNave() {
         showNave = !showNave;
@@ -25,15 +29,21 @@
         on:click={toggleNave}
     />
     <Header on:click={toggleNave} />
-    {#if activeItem === "Home"}
-        <div>Home</div>
-    {:else if activeItem === "Order"}
-        <div>Order</div>
-    {:else if activeItem === "Price List"}
-        <div>Price List</div>
-    {/if}
+    <div class="pages">
+        {#if activeItem === "Home"}
+            <Home />
+        {:else if activeItem === "Order"}
+            <div>Order</div>
+        {:else if activeItem === "Price List"}
+            <div>Price List</div>
+        {/if}
+    </div>
     <Footer />
 </main>
 
 <style>
+    .pages {
+            padding-top: 100px;
+            padding-bottom: 50px;
+        }
 </style>
---
+++ src/components/navePopout/NavePopout.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    import { slide, fly, fade } from "svelte/transition";
+    const dispatch = createEventDispatcher();
+
+    export let items: Array<string>;
+    export let activeItem: string;
+    export let showNave = false;
+</script>
+
+{#if showNave}
+    <div class="backdrop" on:click|self>
+        <div
+            class="block"
+            in:fly={{ x: 200, duration: 200 }}
+            out:fly={{ delay: 300, x: 200, duration: 200 }}
+        >
+            <ul>
+                {#each items as item}
+                    <li
+                        class="tab"
+                        in:slide={{ delay: 220, duration: 250 }}
+                        out:slide={{ duration: 200 }}
+                        on:click={() => dispatch("tabChange", item)}
+                    >
+                        <div class:active={item === activeItem}>{item}</div>
+                    </li>
+                {/each}
+            </ul>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .backdrop {
+        position: fixed;
+        z-index: 2;
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.8);
+        display: flex;
+        justify-content: right;
+    }
+
+    .block {
+        height: 100%;
+        background-color: #adbbce;
+        min-width: 150px;
+    }
+
+    ul {
+        list-style-type: none;
+        margin-top: 60px;
+        padding: 0;
+    }
+
+    .tab {
+        display: flex;
+        color: black;
+        justify-content: center;
+        flex-flow: wrap;
+        font-size: 1.3rem;
+        font-weight: bold;
+        width: 100%;
+        margin: 10px;
+    }
+    .tab:hover {
+        color: #4b4b4b;
+    }
+</style>
