5d8767612d845e1362d09e1a14c2c2879b158384
Jett65 <jettc65@gmail.com>
2023-07-11 17:14:42 -0600

    added api

---
+++ client/src/components/navePopout/NaveButton.svelte
@@ -0,0 +1,35 @@
+<script lang="ts"> 
+</script>
+
+<button class="nave" on:click>
+    <div class="line" />
+    <div class="line" />
+    <div class="line" />
+</button>
+
+<style>
+    .nave {
+        cursor: pointer; 
+        background-color: transparent;
+        border: none;
+        padding: 0;
+        margin: 0;
+        width: 37px;
+        height: 31px;
+        margin-top: 65%; 
+    }
+    .nave:hover {
+        border-radius: 5px;
+        background-color: lightgray;
+    }
+    .line { 
+        background-color: black;
+        display: flex;
+        justify-content: center;
+        width: 95%;
+        height: 6px;
+        border-radius: 10px;
+        margin-top: 1px;
+        margin-bottom: 4px;
+    }
+</style>
---
+++ client/types.ts
@@ -0,0 +1,13 @@
+ export default class Cake {
+    price: number;
+    size: number;
+    shape: string;
+
+    constructor(price: number, size: number, shape: string) {
+        this.price = price;
+        this.size = size;
+        this.shape = shape;
+    }
+}
+
+
